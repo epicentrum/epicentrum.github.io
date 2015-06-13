156e61d381597e2bfd78445ce4f62b792bd6a714
Rafal Trzop <rafaltrzop@gmail.com>
2015-06-13 12:03:36 +0200

    Move slideout script to app.js file. Turn hamburger icon into close icon on opening and closing nav.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// Slideout (https://github.com/Mango/slideout)
+var slideout = new Slideout({
+  'panel': document.getElementById('slideout-panel'),
+  'menu': document.getElementById('slideout-menu'),
+  'padding': 256,
+  'tolerance': 70,
+  'side': "right",
+  'touch': false
+});
+
+// Slideout toggle button
+document.querySelector('#slideout-toggle').addEventListener('click', function() {
+  slideout.toggle();
+});
+
+// Turn ☰ into ×
+slideout.on('beforeopen', function() {
+  $('#slideout-toggle').html("&times;");
+});
+
+slideout.on('beforeclose', function() {
+  $('#slideout-toggle').html("&#9776;");
+});
